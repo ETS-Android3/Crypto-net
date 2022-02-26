ea6a4dcc1cc20673239b0ad9be2e8c04947075ca
Mohamed A. Ibrahim <rageali12@gmail.com>
2022-02-26 19:19:56 +0300

    included location for showDetail Activity's Action bar

---
+++ app/src/main/java/com/moringaschool/cryptonet/ShowdetailActivity.java
@@ -0,0 +1,14 @@
+package com.moringaschool.cryptonet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class showdetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_showdetail);
+    }
+}
