1e0ae94c01f6d1c1fcb3ac896dcf3cdbb446997d
Mohamed A. Ibrahim <rageali12@gmail.com>
2022-02-26 19:26:54 +0300

    created a back/up button in the action bar

--- app/src/main/java/com/moringaschool/cryptonet/ShowdetailActivity.java
+++ app/src/main/java/com/moringaschool/cryptonet/ShowdetailActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class showdetailActivity extends AppCompatActivity {
+public class ShowdetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_showdetail);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
